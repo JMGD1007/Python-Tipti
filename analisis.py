b69cee81d9e6777546a0c523b3d9359da1e140ee
JMGD1007 <jenniferguerrero1098@gmail.com>
2024-06-04 13:56:48 -0500

    analisis2

--- analisis.py
+++ analisis.py
@@ -1,21 +1,20 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from scrapping import obtener_datos
 
-# URL de ejemplo
 url = "https://sommiercenter.com/almohadas"
 
-# Obtener los datos usando la función importada
+# Obtener los datos de "scrapping.py"
 datos = obtener_datos(url)
 
-# Crear un DataFrame con los datos extraídos
+# Crear un DF con los datos extraídos
 df = pd.DataFrame({
-    'NOMBRE': datos['nombres_productos'],
+    'PRODUCTO': datos['nombres_productos'],
     'PRECIO': datos['precios'],
     'PRECIO DE PROMOCION': datos['precios_promo'],
     'DISPONIBILIDAD': datos['disponibilidad']
 })
-
 
 # Limpiar datos
 df.replace('N/A', np.nan, inplace=True)
@@ -28,8 +27,35 @@ df_limpio['PRECIO DE PROMOCION'] = df_limpio['PRECIO DE PROMOCION'].str.replace(
 # Calcular el precio promedio de los productos disponibles
 precio_promedio = df_limpio[df_limpio['DISPONIBILIDAD'] == 'Disponible']['PRECIO'].mean()
 
-# Guardar el df limpio en un archivo CSV
+# Guardar en un archivo CSV
 df_limpio.to_csv('datos_productos_limpios.csv', index=False)
-
-# Imprimir el DataFrame con precios numéricos
 print(df_limpio)
+
+# Análisis Descriptivo
+print("Resumen Estadístico:")
+print(df_limpio.describe())
+
+# Descuento Promedio
+df_limpio['DESCUENTO'] = df_limpio['PRECIO'] - df_limpio['PRECIO DE PROMOCION']
+df_limpio['PORCENTAJE DE DESCUENTO'] = (df_limpio['DESCUENTO'] / df_limpio['PRECIO']) * 100
+descuento_promedio = df_limpio['PORCENTAJE DE DESCUENTO'].mean()
+print(f"\nDescuento Promedio: {descuento_promedio:.2f}%")
+
+# Producto con mayor y menor descuento
+producto_mayor_descuento = df_limpio.loc[df_limpio['PORCENTAJE DE DESCUENTO'].idxmax()]
+producto_menor_descuento = df_limpio.loc[df_limpio['PORCENTAJE DE DESCUENTO'].idxmin()]
+
+print("\nProducto con Mayor Descuento:")
+print(producto_mayor_descuento[['PRODUCTO', 'PRECIO', 'PRECIO DE PROMOCION', 'PORCENTAJE DE DESCUENTO']])
+
+print("\nProducto con Menor Descuento:")
+print(producto_menor_descuento[['PRODUCTO', 'PRECIO', 'PRECIO DE PROMOCION', 'PORCENTAJE DE DESCUENTO']])
+
+# Gráfica de Barras de Precios
+plt.figure(figsize=(14, 7))
+df_limpio.plot(kind='bar', x='PRODUCTO', y=['PRECIO', 'PRECIO DE PROMOCION'], figsize=(14, 7))
+plt.ylabel('Precio')
+plt.title('Comparación de Precios y Precios de Promoción')
+plt.xticks(rotation=90)
+plt.tight_layout()
+plt.show()
